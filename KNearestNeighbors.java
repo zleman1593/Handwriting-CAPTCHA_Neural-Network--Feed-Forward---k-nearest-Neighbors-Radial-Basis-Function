f1992988118c1e9d552547e547ea60f25747ce01
Zackery leman <zleman1593@gmail.com>
2014-05-02 21:43:52 -0400

    Added K. Fixed a few things

--- KNearestNeighbors.java
+++ KNearestNeighbors.java
@@ -24,23 +24,28 @@ public class KNearestNeighbors {
 	public static ArrayList<Integer> hiddenLayerToOutput = new ArrayList<Integer>();
 	public static ArrayList<Double> hiddenLayerDottedOutputValues = new ArrayList<Double>();
 
+	//How many nearest Neighbors to use
+	public static int k;
+	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// usePriorWeights=Boolean.parseBolean(args[4]);
 		// String trainingImages=args[7];
 		// String testingImages=args[8];
 		// String trainingLabels=args[9];
 		// String testingLabels=args[10];
+		// int k = Integer.parseInt(args[11]); 
 
 		// These are hard coded versions of the above
 		String trainingImages = "Training-Images";
 		String testingImages = "Testing-images";
 		String trainingLabels = "Training-Labels";
 		String testingLabels = "Testing-Labels";
+		 k = 3;
 
 		initializeKNearestNeighbours(trainingImages, trainingLabels);
 
 		// Test the test K-Nearest Neighbors Network
-		testKNearestNeighbours(testingImages, testingLabels);
+		testKNearestNeighbours(testingImages, testingLabels, k);
 	}
 
 	public static void initializeKNearestNeighbours(String trainingImages, String trainingLabels) throws IOException {
@@ -67,7 +72,7 @@ public class KNearestNeighbors {
 		long startTime = System.currentTimeMillis();
 		// Initialize weights with values corresponding to the binary pixel value for all nodes in the first hidden layer.
 		// Currently dividing by 2 to only use a half of the training set so we don't run out of memory. We likely don't need that many anyway.
-		for (int i = 0; i < trainingData.size(); i++) {
+		for (int i = 0; i < trainingData.size()/3; i++) {
 			ArrayList<Double> weights = new ArrayList<Double>(numberOfInputNodes);
 			weights = trainingData.get(i).getArrayListData();
 			hiddenLayerNodes.add(weights);
@@ -83,7 +88,7 @@ public class KNearestNeighbors {
 	public static void testKNearestNeighbours(String testingImages, String testingLabels) throws IOException {
 
 		// Loads testing data set
-		DigitImageLoadingService test = new DigitImageLoadingService(testingLabels, testingImages);
+		DigitImageLoadingService test = new DigitImageLoadingService(testingLabels, testingImages, int k);
 		ArrayList<DigitImage> testingData = new ArrayList<DigitImage>();
 		try {
 			// Our data structure holds the testing data
@@ -94,7 +99,7 @@ public class KNearestNeighbors {
 
 		// Tests the network with the testing Data and prints results to file
 		// write(solveTestingData(testingData));
-		solveTestingData(testingData, 10);
+		solveTestingData(testingData, k);
 
 		// reports network Performance
 		double percentCorrect = (countOfCorrectImagesAnalyzed / countOfImagesAnalyzed) * 100;
@@ -145,69 +150,23 @@ public class KNearestNeighbors {
 			ArrayList<Double> temp = networkInputData.get(i).getArrayListData();
 			hiddenLayerDottedOutputValues = outPutOfLayer(hiddenLayerNodes, temp);
 
-			// Find the ten nodes with the highest output value
-		//	QuickSelect o = new QuickSelect();
-		//	int k = hiddenLayerDottedOutputValues.size();
-
-				//FIX-THROWS ERROS----------------------------------------------------------------
-	//The idea here is to find the ten nodes with the highest output values using quicksort in the unsorted ArrayList  "hiddenLayerDotedOutputValues".
-	//Once those nodes are identified (i.e we know the index of the ten nodes with the highest values in the array "hiddenLayerDotedOutputValues")
-	// we will find the corresponding output value (number to guess that) is at the equivalent index in the array "hiddenLayerToOutput".
-	//Whatever number 0-9 is the most common out of those ten numbers indicated, becomes the network output.			
+			??I IF K=1 just run the commented out code as it is faster.	
 				
-				/*		
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-1));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-2));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-3));
-			System.out.println( o.position(hiddenLayerDotedOutputValues,k-4));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-5));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-6));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-7));
-					System.out.println( o.position(hiddenLayerDotedOutputValues,k-8));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-9));
-				System.out.println( o.position(hiddenLayerDotedOutputValues,k-10));
-				System.out.println(" ");
-				*/
-
-				
-				//To easy debugging this will print the the ten guesses of the correct number from ten nearest neighbors
-				
-				/*System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-1)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-2)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-3)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-4)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-5)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-6)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-7)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-8)));
-				System.out.println(hiddenLayerToOutput.get( o.position(hiddenLayerDotedOutputValues,k-9)));*/
-				
-				//Next: Write the code for:
-				//Find what number the majority of ten nodes thinks the input number is closest to and output that guess
-				
-			//FIX: THROWS ERROS----------------------------------------------------------------
-				
-				
-				
-				
-				
-				
-			//The below code between the two lines uses only one nearest neighbor
-			//and will be deleted on the k-nearest Neighbor code is complete.
-			//-----------------------------------------
+			/*	
+			
 				
 			//Find which node has the maximum output and then
 			//return the number that is at that node position in the associated output array.
-			// double currentOutput = 0;
-			// double currentMax = 0;
-			// for (int j = 0; j < hiddenLayerDotedOutputValues.size(); j++) {
-			// 	if (hiddenLayerDotedOutputValues.get(j) > currentMax) {
-			// 		currentMax = hiddenLayerDotedOutputValues.get(j);
-			// 		currentOutput = hiddenLayerToOutput.get(j);
-			// 	}
-			// }
+			 double currentOutput = 0;
+			 double currentMax = 0;
+			 for (int j = 0; j < hiddenLayerDotedOutputValues.size(); j++) {
+				if (hiddenLayerDotedOutputValues.get(j) > currentMax) {
+					currentMax = hiddenLayerDotedOutputValues.get(j);
+					currentOutput = hiddenLayerToOutput.get(j);
+			 	}
+			 }
+			 
+			 */
 			
 			int[] indicesOfDottedOutputList = new int[hiddenLayerDottedOutputValues.size()];
 			ArrayList<Integer> bestKOutputs = new ArrayList<Integer>();
